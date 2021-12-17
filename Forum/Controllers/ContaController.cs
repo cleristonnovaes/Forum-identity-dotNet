c2d5c03b2c7bea7a137db3e95b7ba0b099dcc291
Cleriston <novaes.cleriston@gmail.com>
2021-12-17 16:00:40 -0300

    Criando usuario no DB

---
+++ Forum/Controllers/ContaController.cs
@@ -0,0 +1,42 @@
+﻿using Forum.Models;
+using Forum.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbContext = new IdentityDbContext<UsuarioAplicacao>("DefaultConnection");
+                var userStore = new UserStore<UsuarioAplicacao>(dbContext);
+                var userManager = new UserManager<UsuarioAplicacao>(userStore);
+                var novoUsuario = new UsuarioAplicacao();
+
+                novoUsuario.UserName = modelo.Username;
+                novoUsuario.NomeCompleto = modelo.NomeCompleto;
+                novoUsuario.Email = modelo.Email;
+
+                userManager.Create(novoUsuario, modelo.Senha);
+                
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(modelo);
+        }
+    }
+}
